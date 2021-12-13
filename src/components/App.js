ccf6c3ea3ef9853d1c8d7aa94b11ce143b8981fd
Igor de Moraes <r.de.moraes.igor@gmail.com>
2021-12-13 03:45:59 -0300

    Tela: Inicio

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+      <div class="tela-inicio">
+        <img class="logo" src="./assets/logo.png" />
+        <div class="inicia-rodada" data-identifier="start-zap-recall">
+          <p>Praticar React</p>
+          <img class="next" src="./assets/next.png" />
+        </div>
+      </div>
+    );
+  }
+  
