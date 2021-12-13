941008e727373f6f55eac3737dabb0a8daf7039e
Igor de Moraes <r.de.moraes.igor@gmail.com>
2021-12-13 07:26:36 -0300

    Tela: Flashcards

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
+import TelaInicio from "./TelaInicio";
+
 export default function App() {
-    return (
-      <div class="tela-inicio">
-        <img class="logo" src="./assets/logo.png" />
-        <div class="inicia-rodada" data-identifier="start-zap-recall">
-          <p>Praticar React</p>
-          <img class="next" src="./assets/next.png" />
-        </div>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <TelaInicio />
+    </div>
+  );
+}
